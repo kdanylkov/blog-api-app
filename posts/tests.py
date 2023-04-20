ca7c40f12b9b2cfcf27bdaa0303d4045fee92c7d
Kirill Danilkov <k.danilkov.work@gmail.com>
2023-04-20 18:17:34 +0300

    tests added for posts api

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+from django.contrib.auth import get_user_model
+
+
+class BlogTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+                username='test_user',
+                email='test@email.com',
+                password='secret'
+                )
+        cls.post = Post.objects.create(
+                author=cls.user,
+                title='A good title',
+                body='Nice body content'
+                )
+
+    def test_post_model(self):
+        self.assertEqual(self.post.author.username, 'test_user')
+        self.assertEqual(self.post.title, 'A good title')
+        self.assertEqual(self.post.body, 'Nice body content')
+        self.assertEqual(str(self.post), 'A good title')
